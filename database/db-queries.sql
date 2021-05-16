c29f8d3cec1fd67d1a131e95198bd952de3bff38
sernamar <sernamar@protonmail.com>
2021-05-16 22:23:52 +0200

    Add an example of a INSERT INTO query

--- database/db-queries.sql
+++ database/db-queries.sql
@@ -9,3 +9,13 @@ CREATE TABLE items (
        type TEXT DEFAULT NULL,
        PRIMARY KEY (id)       
 );
+
+-- insert some values
+INSERT INTO items (by, id, kids, parent, text, time, TYPE)
+VALUES ('norvig',
+        2921983,
+        '{ 2922097, 2922429, 2924562, 2922709, 2922573, 2922140, 2922141}',
+        2921506,
+        'Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I''ll keep writing if you keep reading. K?',
+        to_timestamp(1314211127),
+        'comment');

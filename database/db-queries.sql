912235acf0a1bd7ec9ea19c2d32f264de21b059c
sernamar <sernamar@protonmail.com>
2021-05-17 07:54:36 +0200

    Allow duplicates (for now) by using an internal id and item_id

--- database/db-queries.sql
+++ database/db-queries.sql
@@ -1,17 +1,17 @@
 -- create a table to store items (stories, comments, etc.) from HackerNews
 CREATE TABLE items (
+       id SERIAL PRIMARY KEY,
        by TEXT,
-       id SERIAL,
+       item_id INTEGER,
        kids INTEGER[] DEFAULT NULL,
        parent INTEGER DEFAULT NULL,
        text TEXT DEFAULT NULL,
        time TIMESTAMPTZ DEFAULT NULL,
-       type TEXT DEFAULT NULL,
-       PRIMARY KEY (id)       
+       type TEXT DEFAULT NULL
 );
 
 -- insert some values
-INSERT INTO items (by, id, kids, parent, text, time, TYPE)
+INSERT INTO items (by, item_id, kids, parent, text, time, TYPE)
 VALUES ('norvig',
         2921983,
         '{ 2922097, 2922429, 2924562, 2922709, 2922573, 2922140, 2922141}',

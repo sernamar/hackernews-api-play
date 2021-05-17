5b81c8020920627e101f733ab7f3b1ac76a58400
sernamar <sernamar@protonmail.com>
2021-05-17 10:47:16 +0200

    Change the name of the test function to a better one

--- tests/test_hackernews.py
+++ tests/test_hackernews.py
@@ -13,6 +13,6 @@ COMMENT_ID = 2921983
 BODY = """{"by":"norvig","id":2921983,"kids":[2922097,2922429,2924562,2922709,2922573,2922140,2922141],"parent":2921506,"text":"Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I'll keep writing if you keep reading. K?","time":1314211127,"type":"comment"}"""
 
 
-def test_response_text():
+def test_get_item_from_api():
     item = get_item_from_api(COMMENT_ID)
     assert item == BODY

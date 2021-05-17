4e6f6d885bc91d145081d2d126608c05ff663fab
sernamar <sernamar@protonmail.com>
2021-05-17 08:48:07 +0200

    Change name of a function

--- hackernews.py
+++ hackernews.py
@@ -4,7 +4,7 @@ import requests
 BASE_URL = "https://hacker-news.firebaseio.com/v0/item/"
 
 
-def get_comment(id):
+def get_comment_from_api(id):
     url = BASE_URL + str(id) + ".json"
     response = requests.get(url)
     return response.text

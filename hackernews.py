254bf2d602a9e2b5730832fa0a4544917341867e
sernamar <sernamar@protonmail.com>
2021-05-17 19:38:28 +0200

    Add function to get an item from the API and save it to the database

--- hackernews.py
+++ hackernews.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 import psycopg2
@@ -44,7 +45,46 @@ def get_item_from_db(id):
         return item
 
     except (Exception, psycopg2.Error) as error:
-        print("Error while fetching data from PostgreSQL", error)
+        print("Error while fetching data from PostgreSQL:", error)
+
+    finally:
+        # Close the database connection
+        if connection:
+            cursor.close()
+            connection.close()
+            # print("PostgreSQL connection is closed")
+
+
+def save_item_to_db(id):
+    try:
+        # Get item
+        item = get_item_from_api(id)
+
+        # Get the database credentials from the environment variable
+        # DATABASE_URL = os.environ["DATABASE_URL"]
+        DATABASE_URL = "postgresql://user:password@host/database"
+
+        # Connect to an existing database
+        connection = psycopg2.connect(DATABASE_URL)
+
+        # Open a cursor to perform database operations
+        cursor = connection.cursor()
+
+        # Write the query expression
+        query = sql.SQL("""
+INSERT INTO {table} (by, item_id, kids, parent, text, time, type)
+VALUES (%(by)s, %(id)s, %(kids)s, %(parent)s,
+%(text)s, to_timestamp(%(time)s), %(type)s)
+""").format(table=sql.Identifier(TABLE_NAME))
+
+        # Query the database
+        cursor.execute(query, item)
+
+        # Make the changes to the database persistent
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while writing data to PostgreSQL:", error)
 
     finally:
         # Close the database connection
